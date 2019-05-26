9877c1e71911221aca0ed16381a75e1c3351c73a
xlgwr <xlgwr@163.com>
2019-05-26 21:04:33 +0800

    add time fpr demouser

--- models/demouser.go
+++ models/demouser.go
@@ -1,8 +1,12 @@
 package models
 
+import "time"
+
 // DemoUser ..a
 type DemoUser struct {
 	ID       int64
 	Username string
 	Password string
+	Created  time.Time `orm:"auto_now_add;type(datetime)"`
+	Updated  time.Time `orm:"auto_now;type(datetime)"`
 }
